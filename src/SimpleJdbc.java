fddafb0594ff163832b095f9dc8084b61ab40a07
Benjamin Kyhn <benkyhn@hotmail.com>
2019-11-02 16:09:55 +0100

    Først opgave

--- src/SimpleJdbc.java
+++ src/SimpleJdbc.java
@@ -16,35 +16,38 @@ public class SimpleJdbc {
 
         System.out.println("\nDatabase connected.");
 
-        // #2. Create a statement / query / forespørgsel
+        // #2. Create a statement
 
         // ** Med Scanner kan vi få brugeren til at indtaste, hvilket land, de vil se info om
-         Scanner scanner = new Scanner(System.in);
-         System.out.print("Hvilket land vil du se oplysninger om? ±");
-         String landeNavn = scanner.nextLine();
-         String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + landeNavn + "';";
+        String landeNavn = "";
 
-        // ** Uden scanner kan vi kun vise det land, som vi harcoder i query-et
-        // String mitQuery = "SELECT * FROM world.country WHERE name LIKE 'D%';";
-        Statement statement = connection.createStatement();
+        //Fortsæt med at spørge, indtil du skriver "exit"
+        while (!landeNavn.equalsIgnoreCase("exit")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\nHvilket land vil du se oplysninger om? (Skriv 'exit' for at afslutte): ");
+            landeNavn = scanner.nextLine();
 
-        // #3. Execute the statement and send the SQL-query to the SQL-server
-        ResultSet resultSet = statement.executeQuery
-                (mitQuery);
-        System.out.println("Følgende query er sendt til MySQL-serveren: " + mitQuery);
-        System.out.println("Svar fra databasen: " + "\n");
+            String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + '%' + landeNavn + '%' + "';";
+            //String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + landeNavn + "';";
+            // ** Uden scanner kan vi kun vise det land, som vi hardcoder i query-et
+            //String mitQuery = "SELECT * FROM world.country WHERE name LIKE 'D%';";
+            Statement statement = connection.createStatement();
+
+            // #3. Execute the statement and send the SQL-query to the SQL-server
+            ResultSet resultSet = statement.executeQuery
+                    (mitQuery);
+            System.out.println("Følgende query er sendt til MySQL-serveren: " + mitQuery);
+            System.out.println("Svar fra databasen: " + "\n");
 
 
-        int i = 0;
-        // #4. Iterate through the result and print the results (vi har måske flere resultater end 1, derfor vil en løkke læse alle rækker ud fra resultSettet)
+            // #4. Iterate through the result and print the results (vi har måske flere resultater end 1, derfor vil en løkke læse alle rækker ud fra resultSettet)
             while (resultSet.next())
-                i++;
-            System.out.println(resultSet.getString(1) + " " +
-                    resultSet.getString(2) + ", " +
-                    resultSet.getString(3) + ", " +
-                    "Population: " +
-                    resultSet.getString("Population"));
-
+                System.out.println(resultSet.getString(1) + " " +
+                        resultSet.getString(2) + ", " +
+                        resultSet.getString(3) + ", " +
+                        "Population: " +
+                        resultSet.getString("Population"));
+        }
         // #5. Close the connection (always!)
         connection.close();
         System.out.println();
